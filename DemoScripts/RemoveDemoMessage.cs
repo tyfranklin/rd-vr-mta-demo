d84098271752405269d3c90219f52e3b0205044c
Tyler Franklin <get@tylerfranklin.com>
2020-10-05 11:55:24 -0400

    Add demo assets

---
+++ DemoScripts/RemoveDemoMessage.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemoveDemoMessage : MonoBehaviour {
+
+	void Start(){
+		Destroy (this.gameObject);
+		
+		
+	}
+}
---
+++ DemoScripts/TrainPassing.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainPassing : MonoBehaviour {
+
+	GameObject train;
+	public AudioSource mainAudio;
+	float audioVolume;
+
+	public GameObject passingTrainPrefab;
+
+	void Start() {
+		audioVolume = mainAudio.volume;
+	}
+
+	void OnTriggerStay(Collider o) {
+		if (o.tag.Equals ("Player") && train == null) {
+			mainAudio.volume = 0.03f;
+			if (passingTrainPrefab != null) {
+				train = Instantiate (passingTrainPrefab) as GameObject;
+			}
+		}
+	}
+
+	IEnumerator OnTriggerExit(Collider c) {
+		if (c.tag.Equals ("Player")) {
+			mainAudio.volume = audioVolume;
+			yield return new WaitForSeconds (0f);
+			Destroy (train.gameObject);
+		}
+	}
+}
